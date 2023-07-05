c1616aec02a7729a2373e2c621c5844151651466
Nicolas <nicolas.went@epita.fr>
2023-07-05 15:56:49 +0200

    [Types] Config] WIP on config

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ async def main():
                              config["common"]["weth-address"],
                              os.getenv("HTTP_RPC"),
                              config["node-config"]["router-address"])
-    blockchain_apis_runner = BlockchainAPIsRunner()
+    blockchain_apis_runner = BlockchainAPIsRunner(config["token-owned"],
+                                                  config["common"]["weth-address"],
+                                                  config["blockchain-apis-config"]["blockchain-id"],
+                                                  config["blockchain-apis-config"]["exchange-id"],
+                                                  os.getenv("BLOCKCHAIN_APIS_KEY"))
 
     time_spent_node = await node_runner.time_run()
     time_spend_blockchain_apis = await blockchain_apis_runner.time_run()
--- src/Types.py
+++ src/Types.py
@@ -8,12 +8,18 @@ NodeConfig = TypedDict("NodeConfig", {
     "router-address": str
 })
 
+BlockchainAPIsConfig = TypedDict("BlockchainAPIsConfig", {
+    "blockchain-id": str,
+    "exchange-id": str
+})
+
 Config = TypedDict("Config", {
     # Token owned contains as key the address of the token and as value
     # the amount of the token inside of the wallet
     "token-owned": Dict[str, int],
     "common": Common,
-    "node-config": NodeConfig
+    "node-config": NodeConfig,
+    "blockchain-apis-config": BlockchainAPIsConfig
 })
 
 
